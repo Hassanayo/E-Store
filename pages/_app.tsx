7f21f7bfa6688c1d68e04c7fa8d8765abbe875a8
Hassan Ayomide <funyem1710@gmail.com>
2022-07-28 18:44:19 +0100

    responsiveness setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,19 @@ import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
 import { theme, GlobalStyles } from "../src/theme";
 import { CartContextProvider } from "../src/context/CartContext";
+import { ViewportProvider } from "../src/context/viewportContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyles />
-      <CartContextProvider {...pageProps}>
-        <ThemeProvider theme={theme}>
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </CartContextProvider>
+      <ViewportProvider>
+        <CartContextProvider {...pageProps}>
+          <ThemeProvider theme={theme}>
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </CartContextProvider>
+      </ViewportProvider>
     </>
   );
 }
