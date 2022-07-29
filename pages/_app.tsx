6552d794b6686240d9bf5f5fa073c1e0a506c319
Hassan Ayomide <funyem1710@gmail.com>
2022-07-29 12:30:38 +0100

    new fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,8 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
 import { theme, GlobalStyles } from "../src/theme";
-import { ViewportProvider } from "../src/context/viewportContext";
 import { CartContextProvider } from "../src/context/CartContext";
+import { ViewportProvider } from "../src/context/viewportContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
