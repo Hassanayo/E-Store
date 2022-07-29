2e903b103f5ad94ed56b7fe931123928efa355f0
Hassan Ayomide <funyem1710@gmail.com>
2022-07-29 12:12:26 +0100

    client side rendering only

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -3,20 +3,22 @@ import { useViewport } from "../context/viewportContext";
 import MobileHeader from "../MobileComponents/MobileHeader";
 import Footer from "../organisms/Footer";
 import Header from "../organisms/Header";
+import ClientOnly from "../utils/clientOnly";
 import { LayoutWrapper } from "./layout.style";
 
-export default function LayoutBody({ children }: { children: React.ReactNode }) {
-  const {isMobile} = useViewport()
-  return (
-    <LayoutWrapper>
-      <nav>
-        {isMobile ? <MobileHeader/> : <Header/>}
-      </nav>
-      {children}
+export default function LayoutBody({children}: {children: React.ReactNode;}) {
+  const { isMobile } = useViewport();
 
-      <footer>
-        <Footer />
-      </footer>
-    </LayoutWrapper>
+  return (
+    <ClientOnly>
+      <LayoutWrapper>
+        <nav>{isMobile ? <MobileHeader /> : <Header />}</nav>
+        {children}
+
+        <footer>
+          <Footer />
+        </footer>
+      </LayoutWrapper>
+    </ClientOnly>
   );
 }
