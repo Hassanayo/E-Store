8ae814e54950b80446552b37830ef7bbeeb7d716
Hassan Ayomide <funyem1710@gmail.com>
2022-07-29 12:41:10 +0100

    added viewportcontext

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,18 @@ import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
 import { theme, GlobalStyles } from "../src/theme";
 import { CartContextProvider } from "../src/Context/CartContext";
+import { ViewportProvider } from "../src/Context/viewportContext";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyles />
+      <ViewportProvider>
         <CartContextProvider {...pageProps}>
           <ThemeProvider theme={theme}>
             <Component {...pageProps} />
           </ThemeProvider>
         </CartContextProvider>
+      </ViewportProvider>
     </>
   );
 }
