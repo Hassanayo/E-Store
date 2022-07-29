55b59fec677b8e6b00e9914a5a5f0072bb31afbf
Hassan Ayomide <funyem1710@gmail.com>
2022-07-29 12:23:13 +0100

    app.tsx fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,18 +3,17 @@ import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
 import { theme, GlobalStyles } from "../src/theme";
 import { CartContextProvider } from "../src/context/CartContext";
-import { ViewportProvider } from "../src/context/viewportContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyles />
       <CartContextProvider {...pageProps}>
-        <ViewportProvider>
+        
           <ThemeProvider theme={theme}>
             <Component {...pageProps} />
           </ThemeProvider>
-        </ViewportProvider>
+        
       </CartContextProvider>
     </>
   );
